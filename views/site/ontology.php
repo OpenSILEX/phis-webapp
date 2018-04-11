95d05ae794b4be9fd65a98559b493d562fb0722f
vidalmor <morgane.vidal@wanadoo.fr>
2018-04-11 14:08:04 +0200

    add vectors

--- views/site/ontology.php
+++ views/site/ontology.php
@@ -73,52 +73,58 @@ $species = [
             ["label" => "Tensiometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#ElectricalResistanceHumiditySensor"],
         ];
     
-     $capacitanceSensors = [
-            ["label" => "Capacitive Thin Film Polymer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#CapacitiveThinFilmPolymer"],
-        ];
-     
-     $electricalResistanceBlocks = [
-            ["label" => "Fiber Glass Block", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#FiberGlassBlock"],
-            ["label" => "Gypsum Block", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#GypsumBlock"],
-            ["label" => "Nylon Units", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#NylonUnits"],
-        ];
-     
-      $precipitationSensors = [
-           ["label" => "Level Measurement Rain Gauge", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#LevelMeasurementRainGauge"],
-        ];
-      
-      $radiationSensors = [
-            ["label" => "Net Radiometer Or Net Pyrradiometer Or Net Exchange Radiometer Or Balancemeter", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#NetRadiometerOrNetPyrradiometerOrNetExchangeRadiometerOrBalancemeter"],
-            ["label" => "Pyranometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#Pyranometer"],
-            ["label" => "Pyranometer With Shade Ring", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#PyranometerWithShadeRing"],
-            ["label" => "Quantum Sensor", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#QuantumSensor"],
-            ["label" => "Surface Temperature Radiometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#SurfaceTemperatureRadiometer"],
-        ];
-      
-        $spectroradiometers = [
-            ["label" => "Green Seeker", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#GreenSeeker"],
-        ];
-        
-        $temperatureSensors = [
-            ["label" => "Electrical Resistance Thermometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#ElectricalResistanceThermometer"],
-            ["label" => "Thermistor", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#Thermistor"],
-            ["label" => "Thermocouple", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#Thermocouple"],
-        ];
-        
-        $windSensors = [
-            ["label" => "CupAnemometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#Tensiometer"],
-            ["label" => "Propeller Anemometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#PropellerAnemometer"],
-            ["label" => "Ultrasonic Wind Sensor", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#UltrasonicWindSensor"],
-        ];
-        
-        $weighingScale = [
-            ["label" => "Weighing Scale", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#WeighingScale"],
-        ];
+    $capacitanceSensors = [
+           ["label" => "Capacitive Thin Film Polymer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#CapacitiveThinFilmPolymer"],
+       ];
+
+    $electricalResistanceBlocks = [
+           ["label" => "Fiber Glass Block", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#FiberGlassBlock"],
+           ["label" => "Gypsum Block", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#GypsumBlock"],
+           ["label" => "Nylon Units", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#NylonUnits"],
+       ];
+
+     $precipitationSensors = [
+          ["label" => "Level Measurement Rain Gauge", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#LevelMeasurementRainGauge"],
+       ];
+
+     $radiationSensors = [
+           ["label" => "Net Radiometer Or Net Pyrradiometer Or Net Exchange Radiometer Or Balancemeter", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#NetRadiometerOrNetPyrradiometerOrNetExchangeRadiometerOrBalancemeter"],
+           ["label" => "Pyranometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#Pyranometer"],
+           ["label" => "Pyranometer With Shade Ring", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#PyranometerWithShadeRing"],
+           ["label" => "Quantum Sensor", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#QuantumSensor"],
+           ["label" => "Surface Temperature Radiometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#SurfaceTemperatureRadiometer"],
+       ];
+
+       $spectroradiometers = [
+           ["label" => "Green Seeker", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#GreenSeeker"],
+       ];
+
+       $temperatureSensors = [
+           ["label" => "Electrical Resistance Thermometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#ElectricalResistanceThermometer"],
+           ["label" => "Thermistor", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#Thermistor"],
+           ["label" => "Thermocouple", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#Thermocouple"],
+       ];
+
+       $windSensors = [
+           ["label" => "CupAnemometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#Tensiometer"],
+           ["label" => "Propeller Anemometer", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#PropellerAnemometer"],
+           ["label" => "Ultrasonic Wind Sensor", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#UltrasonicWindSensor"],
+       ];
+
+       $weighingScale = [
+           ["label" => "Weighing Scale", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#WeighingScale"],
+       ];
 
     $sensingDevicesNumber = count($cameras) + count($humiditySensors) + count($capacitanceSensors) 
             + count($electricalResistanceBlocks) + count($precipitationSensors) + count($radiationSensors)
             + count($spectroradiometers) + count($temperatureSensors) + count($sensingDevices)
             + count($windSensors);
+    
+    $vectors = [
+        ["label" => "Field Robot", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#FieldRobot"],
+        ["label" => "Gantry", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#Gantry"],
+        ["label" => "UAV", "uri" => "http://www.phenome-fppn.fr/vocabulary/2017#UAV"]
+    ]
     
 ?>
 
@@ -173,6 +179,10 @@ $species = [
                     <span class="badge badge-primary badge-pill"><?= Html::encode(count($windSensors)) ?></span>
                 </li>
             </ul>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <?= Html::a(Yii::t('yii', 'Vectors'), ['ontology', '#' => 'vectors']) ?>
+            <span class="badge badge-primary badge-pill"><?= Html::encode(count($vectors)) ?></span>
         </li>
     </ul>
     
@@ -402,6 +412,25 @@ $species = [
 
         echo yii\grid\GridView::widget([
             'dataProvider' => $windSensorsProvider,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                'label',
+                'uri'
+            ]
+        ]);   
+    ?>
+    
+    <h2 id="vectors"><?= Html::encode(Yii::t('app', 'Vectors')) ?></h2>
+    <?php
+        $vectorsProvider = new \yii\data\ArrayDataProvider([
+           'allModels' => $vectors,
+           'sort' => [
+               'attributes' => ['label', 'uri']
+           ]
+        ]);        
+
+        echo yii\grid\GridView::widget([
+            'dataProvider' => $vectorsProvider,
             'columns' => [
                 ['class' => 'yii\grid\SerialColumn'],
                 'label',
