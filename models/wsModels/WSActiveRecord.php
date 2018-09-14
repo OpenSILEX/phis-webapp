1d5c9fd7b8c5a08856fdac2eb7e80ef75ba55c20
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2018-09-14 17:12:18 +0200

    Show 1 items on 1 when only 1 item is returned

--- models/wsModels/WSActiveRecord.php
+++ models/wsModels/WSActiveRecord.php
@@ -1,26 +1,22 @@
 <?php
-
-//**********************************************************************************************
-//                                       WSActiveRecord.php 
-//
-// Author(s): Morgane VIDAL
-// PHIS-SILEX version 1.0
-// Copyright © - INRA - 2017
-// Creation date: February 2017
-// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
-// Last modification date:  February, 2017
-// Subject: An adapted Active Record based to request web services
-//          Based on the Yii Active Record of relational databases
-//          See Yii2 ActiveRecord documentation for more details
-//***********************************************************************************************
-
+//******************************************************************************
+//                         WSActiveRecord.php
+// SILEX-PHIS
+// Copyright © INRA 2017
+// Creation date:  Feb, 2017
+// Contact: arnaud.charleroy@inra.fr,  morgane.vidal@inra.fr, anne.tireau@inra.fr,
+//          pascal.neveu@inra.fr
+//******************************************************************************
 namespace app\models\wsModels;
 
 /**
  * An active record for the web services. 
- * Adapted from the Yii2's ActiveRecord developped for relational databases
+ * An adapted Active Record based to request web services
+ * Based on the Yii Active Record of relational databases
+ * See Yii2 ActiveRecord documentation for more details
  * @see http://www.yiiframework.com/doc-2.0/guide-db-active-record.html
- * @author Morgane Vidal <morgane.vidal@inra.fr>
+ * @author Morgane Vidal <morgane.vidal@inra.fr>, Arnaud Charleroy <arnaud.charleroy@inra.fr>
+ * @update [Arnaud Charleroy] 14 September, 2018 : Fix TotalCount when only one element is returned 
  */
 abstract class WSActiveRecord extends \yii\base\Model {
     
@@ -116,6 +112,7 @@ abstract class WSActiveRecord extends \yii\base\Model {
         }
         
         if (isset($requestRes->{WSConstants::RESULT}->{WSConstants::DATA}))  {
+            $this->totalCount = 1;
             return (array) $requestRes->{WSConstants::RESULT}->{WSConstants::DATA};
             
         } else {
