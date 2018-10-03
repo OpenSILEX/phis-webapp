e79257f729fc1f3015a90bdf304b674cb490ba93
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2018-10-03 02:27:55 +0200

    Add comment on code

--- views/site/index.php
+++ views/site/index.php
@@ -1,12 +1,30 @@
 <?php
-
 /* @var $this yii\web\View */
 $this->title = 'PHIS';
+
 ?>
+
 <div class="site-index">
     <div class="jumbotron">
         <img src="images/logos/phis_logo10.png" alt="logos_phis"/>
         <h3>You are on PHIS, the Hybrid Information System about Phenotyping !</h3>
         <!--<h3>Yii::t('app/messages', 'Intro'); </h3>-->
     </div>
+    <?php    
+//    SILEX:info
+//    If you want to activate animated multiple background images 
+//    feature uncomment these lines below and add
+//    'use app\components\widgets\FullScreenImageSliderWidget;'
+//    in the import. you can also see full-slider.css to set different 
+//    annimation style.
+//    \SILEX:info
+//        echo FullScreenImageSliderWidget::widget([
+//            FullScreenImageSliderWidget::IMAGES_URL_LINK => [
+//                "background/wallpaper_grapes_vine.jpg",
+//                "background/wallpaper_leaf.jpg",
+//                "background/wallpaper_tomato.jpg",
+//                "background/wallpaper_vine.jpg"
+//            ]
+//        ]);
+    ?>
 </div>
