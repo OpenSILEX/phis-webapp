b90b7893fd4c235feca73c04610e879146a93130
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2018-10-08 09:31:04 +0200

    Cleaning

---
+++ config/web_services.php
@@ -0,0 +1,16 @@
+<?php
+
+//**********************************************************************************************
+//                                       web_services.php 
+//
+// Author(s): Morgane VIDAL
+// PHIS-SILEX version 1.0
+// Copyright © - INRA - 2017
+// Creation date: February 2017
+// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+// Last modification date:  February, 2017
+// Subject: Contains the web services access
+//***********************************************************************************************
+
+define('WS_PHIS_PATH', 'http://localhost:8084/phis-ws/rest/');
+define('WS_PHIS_PATH_DOC', 'http://localhost:8084/phis2ws');
