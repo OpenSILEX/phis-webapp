9f2ff668f0935c636ec6dca1c155cf7b841549e5
Andréas <andreas.garcia@lilo.org>
2019-01-23 08:44:38 +0100

    Remove temporally type filter because of the URI control issue

--- views/event/index.php
+++ views/event/index.php
@@ -27,7 +27,12 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-            'type',
+            [
+                'attribute' => 'type',
+                'format' => 'raw',
+                'value' => 'type',
+                'filter' => false
+            ],
             [
                 'attribute' => 'concernedItemLabel',
                 'format' => 'raw',
