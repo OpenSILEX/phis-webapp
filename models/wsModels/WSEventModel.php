7bb3ff42387ace4943d00121b9b2095171c177f8
Andréas <andreas.garcia@lilo.org>
2019-01-25 15:14:21 +0100

    Improve coding style

--- models/wsModels/WSEventModel.php
+++ models/wsModels/WSEventModel.php
@@ -1,16 +1,16 @@
 <?php
 
 //******************************************************************************
-//                                       WSEventModel.php
-//
-// Author(s): Andréas Garcia <andreas.garcia@inra.fr>
-// PHIS-SILEX version 1.0
-// Copyright © - INRA - 2018
-// Creation date: 02 janvier 2019
+//                               WSEventModel.php
+// PHIS-SILEX
+// Copyright © INRA 2018
+// Creation date: 02 jan. 2019
 // Contact: andreas.garcia@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
 //******************************************************************************
 
 namespace app\models\wsModels;
+
+use \openSILEX\guzzleClientPHP\WSModel;
 
 include_once '../config/web_services.php';
 
@@ -19,10 +19,10 @@ include_once '../config/web_services.php';
  * @see \openSILEX\guzzleClientPHP\WSModel
  * @author Andréas Garcia <andreas.garcia@inra.fr>
  */
-class WSEventModel extends \openSILEX\guzzleClientPHP\WSModel {
+class WSEventModel extends WSModel {
     
     /**
-     * initialize access to the events service. Calls super constructor
+     * Initialize access to the events service. Calls super constructor
      */
     public function __construct() {
         parent::__construct(WS_PHIS_PATH, "events");
