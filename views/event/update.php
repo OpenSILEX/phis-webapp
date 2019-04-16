838aba12658d6428eb938452af36bcac25740732
Andréas <andreas.garcia@lilo.org>
2019-04-16 16:09:05 +0200

    Add event update view

---
+++ views/event/update.php
@@ -0,0 +1,27 @@
+<?php
+
+//******************************************************************************
+//                                       update.php
+// SILEX-PHIS
+// Copyright © INRA 2018
+// Creation date: 18 oct. 2018
+// Contact: vincent.migot@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+//******************************************************************************
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\YiiRadiometricTargetModel */
+
+$this->title = Yii::t('yii', 'Update Radiometric Target');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', '{n, plural, =1{Radiometric Target} other{Radiometric Targets}}', ['n' => 2]), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="radiometric-target-create">
+    <h1><?= Html::encode($this->title) ?></h1>
+    
+    <?= $this->render('_form', [
+        'model' => $model,
+        'hideFiles' => true
+    ]) ?>
+</div>
