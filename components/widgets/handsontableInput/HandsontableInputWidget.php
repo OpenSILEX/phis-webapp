832011277f3846801a344a49519a5d4e893ef3ec
Andréas <andreas.garcia@lilo.org>
2019-05-06 14:40:11 +0200

    Move script in js file

--- components/widgets/handsontableInput/HandsontableInputWidget.php
+++ components/widgets/handsontableInput/HandsontableInputWidget.php
@@ -6,71 +6,48 @@
 // Creation date: 15 Apr. 2019
 // Contact: andreas.garcia@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
 //******************************************************************************
-namespace app\components\widgets;
+namespace app\components\widgets\handsontableInput;
+
 use yii\helpers\Html;
 use himiklab\handsontable\HandsontableWidget;
+use app\components\widgets\handsontableInput\HandsontableInputWidgetAsset;
+use yii\web\View;
+
 /**
  * Handsontable grid input widget for Yii2.
- * The input is a hidden input that stores the JSON-encoded string representation of the grid's data.
- *
- * ```php
- * $this->widget('\neam\yii_handsontable_input\widgets\HandsontableInput',[
- *  'id' => 'foo',
- *  'value' => [
- *          ['A1', 'B1', 'C1'],
- *          ['A2', 'B2', 'C2'],
- *  ],
- *  'settings' => [
- *    'colHeaders' => true,
- *    'rowHeaders' => true,
- *  ]
- * ]);
- * ```
  */
 class HandsontableInputWidget extends HandsontableWidget
 {
     const INPUT_GROUP_DIV_ID = "handsontable-inputs-group";
-    const ACTION_BUTTONS_GROUP_DIV = "handsontable-action-buttons-group";
+    const ACTION_BUTTONS_GROUP_DIV_CLASS = "handsontable-action-buttons-group";
     const ADD_ROW_BUTTON_ID = "handsontable-add-row-button";
     const DELETE_ROW_BUTTON_ID = "handsontable-delete-row-button";
     
     public $inputName;
+    public $title;
     
     public function run()
-    {     
+    {      
+        if($title) {
+            echo "";
+        }
         echo $this->renderActionButtons() . " " . $this->renderInput();
         
-        parent::run();        
+        parent::run();
         
-        $this->view->registerJs("
-            
-        var form = document.querySelector('form');
-        var inputName = '{$this->inputName}';
-        form.onsubmit = function() {
-            var inputsGroup  = document.querySelector('#" . self::INPUT_GROUP_DIV_ID . "');
-            inputsGroup.innerHTML = '';
-            var tds = document.querySelectorAll('.htCore td');
-            tds.forEach(function(td) {
-                var input = document.createElement('input');  
-                input.setAttribute('name', inputName);
-                input.setAttribute('value', td.innerHTML);
-                inputsGroup.appendChild(input);
-            });
-        };
+        $this->getView()->registerJs(""
+                . "var inputName = '{$this->inputName}';"
+                . "var inputGroupDivId = '" . self::INPUT_GROUP_DIV_ID . "';"
+                . "var addRowButtonId = '" . self::ADD_ROW_BUTTON_ID . "';"
+                . "var deleteRowButtonId = '" . self::DELETE_ROW_BUTTON_ID . "';"
+                . "var handsonTable;"
+                . "", View::POS_HEAD);
         
-        var addRowButton = document.getElementById('" . self::ADD_ROW_BUTTON_ID . "');
-        var deleteRowButton = document.getElementById('" . self::DELETE_ROW_BUTTON_ID . "'); 
-
-        Handsontable.dom.addEvent(addRowButton, 'click', function () {
-            {$this->jsVarName}.alter('insert_row', 0);
-        });
-
-        Handsontable.dom.addEvent(deleteRowButton, 'click', function () {
-            var rowCount = {$this->jsVarName}.countRows();
-            {$this->jsVarName}.alter('remove_row', rowCount - 1);
-        });
+        $this->getView()->registerJs(""
+                . "handsonTable = {$this->jsVarName};"
+                . "", View::POS_READY);
         
-        ", \yii\web\View::POS_READY);
+        HandsontableInputWidgetAsset::register($this->getView());
     }
     
     protected function renderInput () {
@@ -79,7 +56,7 @@ class HandsontableInputWidget extends HandsontableWidget
     
     protected function renderActionButtons () {
         return  
-            "<div id=\"" . self::ACTION_BUTTONS_GROUP_DIV . "\">"
+            "<div class=\"" . self::ACTION_BUTTONS_GROUP_DIV_CLASS . "\">"
                 . Html::buttonInput("Add row", [
                     'id' => self::ADD_ROW_BUTTON_ID,
                     'class' => "btn btn-primary"
