b6255133d597157c796aefd7f5e85245ad43e9b9
Pierre-Etienne Alary <pierre-etienne.alary@inra.fr>
2019-05-24 11:42:59 +0200

    bug fixed by changing messages to app/messages

--- views/site/contact.php
+++ views/site/contact.php
@@ -14,19 +14,19 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-      <?= Yii::t('messages', 'You wish to report a bug or to get help ? OpenSILEX development team can be contacted through the email address') ?>
+      <?= Yii::t('app/messages', 'You wish to report a bug or to get help ? OpenSILEX development team can be contacted through the email address') ?>
       <a href="mailto:opensilex-help@groupes.renater.fr"> opensilex-help@groupes.renater.fr</a>.
     </p>
 
     <p>
-      <?= Yii::t('messages', 'You wish to get notified when new developments are available ? You can follow us on') ?>
+      <?= Yii::t('app/messages', 'You wish to get notified when new developments are available ? You can follow us on') ?>
       <a href="https://twitter.com/OpenSilex"> <?= Yii::t('app', 'twitter') ?></a>
-      <?= Yii::t('messages', 'or subscribe to one of the following mailing list :') ?>
+      <?= Yii::t('app/messages', 'or subscribe to one of the following mailing list :') ?>
       <ul>
         <li><a href="https://groupes.renater.fr/sympa/info/opensilex"> OpenSILEX</a>,
-          <?= Yii::t('messages', 'for generic information about the latest news (training sessions, new releases, ...)') ?></li>
+          <?= Yii::t('app/messages', 'for generic information about the latest news (training sessions, new releases, ...)') ?></li>
         <li><a href="https://groupes.renater.fr/sympa/info/opensilex-dev"> OpenSILEX-dev</a>,
-          <?= Yii::t('messages', 'for technical information directed to OpenSILEX contributors') ?></li>
+          <?= Yii::t('app/messages', 'for technical information directed to OpenSILEX contributors') ?></li>
     </p>
 
 </div>
