6484ae5a21af377b31a4bd9d9fa46ff8cb5ae9d1
Pierre-Etienne Alary <pierre-etienne.alary@inra.fr>
2019-05-24 11:35:08 +0200

    welcome message in index translated

--- views/site/index.php
+++ views/site/index.php
@@ -9,12 +9,16 @@ $this->title = Yii::$app->params['opensilex-webapp-type'];
         if (Yii::$app->params['opensilex-webapp-type'] === 'phis') {
             ?>
             <img src="images/logos/phis_logo10.png" alt="logos_phis"/>
-            <h3>You are on PHIS, the Hybrid Information System about Phenotyping !</h3>
+            <h3>
+              <?= Yii::t('messages', 'You are on PHIS, the Hybrid Information System about Phenotyping!') ?>
+            </h3>
             <?php
         } else {
             ?>
             <img src="images/logos/opensilex_logo_showcase-site.png" alt="logos_opensilex"/>
-            <h3>You are on OpenSILEX, the Hybrid Information System about Life Science !</h3>
+            <h3>
+              <?= Yii::t('messages', 'You are on PHIS, the Hybrid Information System about Phenotyping!') ?>
+            </h3>
             <br />
             <div class="btns">
               <a href="https://twitter.com/OpenSilex"><?=  Icon::show('twitter', ['class' => 'fa-large'], Icon::FA) ?> Twitter</a>
