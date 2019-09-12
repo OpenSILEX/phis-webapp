d75ac9dad61f1d079fc78f9da28f0119f3867e5f
jbonnefont <jbonnefont2@gmail.com>
2019-09-12 17:23:58 +0200

    Merge branch 'cart' into dev/master
    # Conflicts:
    #	config/web_services.php

---
+++ config/web_services.php
@@ -0,0 +1,12 @@
+<?php
+
+//**********************************************************************************************
+//                                       web_services.php
+// SILEX-PHIS
+// Copyright © INRA 2017
+// Creation date: February 2017
+// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+//***********************************************************************************************
+
+define('WS_PHIS_PATH', 'http://localhost:8080/opensilex/rest/');
+define('WS_PHIS_PATH_DOC', 'http://localhost:8080/opensilex/');
