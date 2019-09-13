34b5eac412f419d037c776af6b4989f7d7631562
jbonnefont <jbonnefont2@gmail.com>
2019-09-13 10:24:57 +0200

    Fix missing scientific object on event creation

--- controllers/EventController.php
+++ controllers/EventController.php
@@ -185,7 +185,7 @@ class EventController extends GenericController {
         if (!$event->load(Yii::$app->request->post())) {
             $event->load(Yii::$app->request->get(), '');
             if(Yii::$app->request->get()['type'] === "scientific-objects"){
-                 $event->load(array(self::PARAM_CONCERNED_ITEMS_URIS =>Yii::$app->session['cart']),'');
+                 $event->load(array(self::PARAM_CONCERNED_ITEMS_URIS =>Yii::$app->session['scientific-object']),'');
             }
             $event->creator = $this->getCreatorUri($sessionToken);
             $this->loadFormParams();
