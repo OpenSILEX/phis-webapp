a655e55a3421c7e36111bb66bc2e6949fd7b6176
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2019-12-18 15:17:09 +0100

    Update provenance model

--- views/dataset/_form.php
+++ views/dataset/_form.php
@@ -188,7 +188,12 @@ $this->registerCssFile("https://rawgit.com/lykmapipo/themify-icons/master/css/th
 
                 // Set provenance provenanceAgents, disable input and remove validation messages 
                 try {
-                    var provenanceAgents = provenances[uri]["metadata"]["prov:Agent"]["oeso:Operator"];
+                    var provenanceAgents = [];
+                    provenances[uri]["metadata"]["prov:Agent"].forEach(function(agentElement){
+                        if(agentElement.hasOwnProperty("oeso:Operator")){
+                            provenanceAgents.push(agentElement["oeso:Operator"]);
+                        }
+                      });
                     $("#yiidatasetmodel-provenanceagents").val(provenanceAgents);
                     $("#yiidatasetmodel-provenanceagents").trigger("change");
                 } catch (error) {
