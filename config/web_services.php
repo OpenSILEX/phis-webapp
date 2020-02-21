02cfbfa0254ba9b7edaf6cfe5731361a6859a663
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2020-02-21 10:45:05 +0100

    fix web service config on dev-master

--- config/web_services.php
+++ config/web_services.php
@@ -8,5 +8,5 @@
 // Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
 //***********************************************************************************************
 
-define('WS_PHIS_PATH', 'http://138.102.159.37:8081/rest/');
-define('WS_PHIS_PATH_DOC', 'http://138.102.159.37:8081/');
+define('WS_PHIS_PATH', 'http://138.102.159.37:8080/openSilexTestAPI/rest');
+define('WS_PHIS_PATH_DOC', 'http://138.102.159.37:8080/openSilexTestAPI');
