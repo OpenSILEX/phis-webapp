c106ed52962282965cf74c62e7474b3f0c87ff9e
jbonnefont <jbonnefont2@gmail.com>
2020-03-05 10:45:37 +0100

    remove position on image

--- views/image/_simple_images_visualization.php
+++ views/image/_simple_images_visualization.php
@@ -51,7 +51,7 @@ use yii\helpers\Url;
                 echo Html::img($url, [
                     'width' => 200,
                     'data-html' => 'true',
-                    'title' => 'Position: ' . $position . '<br>Date: ' . $date,
+                    'title' => 'Date: ' . $date,
                     'data-toggle' => 'tooltip',
                     'data-placement' => 'bottom',
                     'data-serie'=>$serieIndex,
